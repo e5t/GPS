c93c45dfd19905f68cb288ebdcf70e57dec49bfc
Jason <e5t@yahoo.com>
2019-05-20 02:03:40 -0600

    Added to .gitignore file

--- pygps.py
+++ pygps.py
@@ -1,26 +1,62 @@
-import serial 
+# Based on the Python GPS example given by SparkFun Electronics [A.Weiss]
 
-# ser = 0 
 
-# def init_serial():
-#     print('Found Ports:')
-#     for n,s in scan():
-#         print(s)
-#     print(' ')
+import serial
+from serial.tools import list_ports
+from pynmea import nmea
 
+# ===========  Global Variables   ===========
+# =   Declare as 'global in the function    =
+ser = 0
+
+# ===========  Constants    ===========
+BAUDRATE = 4800
+COMPORT = 'COM3'
 
 def scan():
-    # available = []
-    for i in range(256):
-        try:
-            s = serial.Serial("COM4")
-            #available.append((i, s.name))
-            print(s.name)
-            s.close()
-        except serial.SerialException:
-            pass
-    return available
+    print([comport.device for comport in serial.tools.list_ports.comports()])
 
-#vinit_serial()
+
+def init_serial():
+    global ser
+    ser = serial.Serial(
+    port = COMPORT, 
+    baudrate = BAUDRATE, 
+    timeout =1
+    )
+
+
+def close_serial():
+    ser.close()
+
+
+        
+
+def stream_serial():
+    # stream data directly from the serial port
+    while True:
+        line = ser.readline()
+        line_str = str(line)
+        print(line_str)
+
+
+def stream_position():
+    # while True:
+        for i in range(300):
+            line = ser.readline()
+            line_str = line.decode('utf8')
+            # print(line_str)
+            if(line_str[4] == 'G'):
+                 print(line_str)
+        
+
+
+
+
+
 scan()
-
+init_serial()
+# stream_serial()
+stream_position()
+close_serial()
+ser.close()
